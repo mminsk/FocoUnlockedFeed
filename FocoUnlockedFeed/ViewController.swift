98371903bc6309770c289005943538128eb06a5e
Madison Minsk <madisonminsk@madisons-air.wireless.dartmouth.edu>
2016-03-08 16:37:02 -0500

    Initial Commit

---
+++ FocoUnlockedFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FocoUnlockedFeed
+//
+//  Created by Madison Minsk on 3/8/16.
+//  Copyright © 2016 Madison Minsk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
